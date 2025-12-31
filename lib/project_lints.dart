fb6eaa3836e4030d67861feed99cd59f3fd03340
hieuvnd-amcolab <hieuvnd@amcolab.vn>
2025-12-31 09:24:57 +0700

    xxxx

--- lib/project_lints.dart
+++ lib/project_lints.dart
@@ -1,18 +1,24 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:project_lints/rules/no_sized_box_shrink.dart';
+import 'package:project_lints/rules/prefer_asset_extension.dart';
 import 'package:project_lints/rules/prefer_context_focus.dart';
 import 'package:project_lints/rules/prefer_context_media_query.dart';
 import 'package:project_lints/rules/prefer_context_theme.dart';
+import 'package:project_lints/rules/prefer_sized_box_extension.dart';
+import 'package:project_lints/rules/prefer_tap_extension.dart';
 
 PluginBase createPlugin() => _ProjectLints();
 
 class _ProjectLints extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
+    // Widget rules
     NoSizedBoxShrinkRule(),
-    PreferContextThemeRule(),
-    PreferContextMediaQueryRule(),
-    PreferContextFocusRule(),
+    PreferSizedBoxExtensionRule(),
+    PreferTapExtensionRule(),
+    PreferAssetExtensionRule(),
+
+    // Context extension rules
     PreferContextThemeRule(),
     PreferContextMediaQueryRule(),
     PreferContextFocusRule(),
