f19a5224ffd37590dffb22c8c87c9b97c153b385
hieuvnd-amcolab <hieuvnd@amcolab.vn>
2025-12-31 11:16:27 +0700

    x

--- lib/rules/prefer_dot_shorthand.dart
+++ lib/rules/prefer_dot_shorthand.dart
@@ -58,10 +58,15 @@ class PreferDotShorthandRule extends DartLintRule {
   ) {
     context.registry.addPrefixedIdentifier((node) {
       final prefix = node.prefix;
-
       final enumName = prefix.name;
 
+      if (_isInSwitchCase(node)) {
+        reporter.atNode(node, _code);
+        return;
+      }
+
       if (!_flutterEnums.contains(enumName)) return;
+      if (_isInConstContext(node)) return;
 
       if (_canUseShorthand(node)) {
         reporter.atNode(node, _code);
@@ -74,7 +79,13 @@ class PreferDotShorthandRule extends DartLintRule {
 
       final enumName = target.name;
 
+      if (_isInSwitchCase(node)) {
+        reporter.atNode(node, _code);
+        return;
+      }
+
       if (!_flutterEnums.contains(enumName)) return;
+      if (_isInConstContext(node)) return;
 
       if (_canUseShorthand(node)) {
         reporter.atNode(node, _code);
@@ -82,15 +93,57 @@ class PreferDotShorthandRule extends DartLintRule {
     });
   }
 
+  bool _isInSwitchCase(AstNode node) {
+    AstNode? current = node;
+
+    while (current != null) {
+      if (current is SwitchCase) return true;
+
+      if (current is SwitchPatternCase) return true;
+
+      if (current is SwitchExpressionCase) return true;
+
+      current = current.parent;
+    }
+
+    return false;
+  }
+
+  bool _isInConstContext(AstNode node) {
+    AstNode? current = node;
+
+    while (current != null) {
+      if (current is InstanceCreationExpression && current.keyword != null) {
+        if (current.keyword!.lexeme == 'const') return true;
+      }
+
+      if (current is DefaultFormalParameter) return true;
+
+      if (current is VariableDeclaration) {
+        final parent = current.parent;
+        if (parent is VariableDeclarationList && parent.keyword != null) {
+          if (parent.keyword!.lexeme == 'const') return true;
+        }
+      }
+
+      if (current is Annotation) return true;
+
+      if (current is ListLiteral && current.constKeyword != null) return true;
+      if (current is SetOrMapLiteral && current.constKeyword != null)
+        return true;
+
+      current = current.parent;
+    }
+
+    return false;
+  }
+
   bool _canUseShorthand(AstNode node) {
     final parent = node.parent;
 
     if (parent is NamedExpression) return true;
-
     if (parent is VariableDeclaration) return true;
-
     if (parent is DefaultFormalParameter) return true;
-
     if (parent is ReturnStatement) return true;
     if (parent is ListLiteral || parent is SetOrMapLiteral) return true;
     if (parent is ArgumentList) return true;
