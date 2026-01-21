c4ca2501fd28a2413a93643e1f211ccc030a608c
hieuvnd-amcolab <hieuvnd@amcolab.vn>
2026-01-21 12:02:19 +0700

    Update context_ext.dart

--- lib/extensions/context_ext.dart
+++ lib/extensions/context_ext.dart
@@ -7,5 +7,5 @@ extension BuildContexExt on BuildContext {
   double get height => MediaQuery.of(this).size.height;
   double get viewInsets => MediaQuery.of(this).viewInsets.bottom;
   SizedBox get empty => const SizedBox.shrink();
-  void unfocus() => FocusScope.of(this).unfocus();
+void unfocus() => FocusScope.of(this).requestFocus(FocusNode());
 }
